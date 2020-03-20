7ba4411a474b4314385a559fcf6bf8a3bdc05b8c
priety33 <52635177+priety33@users.noreply.github.com>
2020-03-20 13:32:55 +0530

    Create Building Bridges.cpp

---
+++ Building Bridges.cpp
@@ -0,0 +1,6 @@
+Approach: It is a variation of LIS problem. The following are the steps to solve the problem.
+
+    Sort the north-south pairs on the basis of increasing order of south x-coordinates.
+    If two south x-coordinates are same, then sort on the basis of increasing order of north x-coordinates.
+    Now find the Longest Increasing Subsequence of the north x-coordinates.
+    One thing to note that in the increasing subsequence a value can be greater as well as can be equal to its previous value.
