f92203f0d72f8cb0007e57ad75cfb82b96fe87fb
priety33 <52635177+priety33@users.noreply.github.com>
2020-03-20 12:30:06 +0530

    Create Minimum number of increasing subsequences.cpp

---
+++ Minimum number of increasing subsequences.cpp
@@ -0,0 +1,3 @@
+/*  Minimum number of increasing subsequences equals to the length of longest decreasing subsequence where
+each element from the longest decreasing subsequence represents an increasing subsequence, so it can be found
+in N*Log(N) time complexity in the same way as longest increasing subsequence by multiplying all the elements with -1. */
