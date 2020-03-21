54030148b7e53be56512030d57c5a395c7ec2193
priety33 <52635177+priety33@users.noreply.github.com>
2020-03-21 15:13:06 +0530

    Update Maximum sum increasing subsequence.cpp

--- Maximum sum increasing subsequence.cpp
+++ Maximum sum increasing subsequence.cpp
@@ -27,3 +27,24 @@ int main() {
 	}
 	return 0;
 }
+
+//O(nlogn)- similar to activity selection problem, greedy
+
+bool comp(struct val a,struct val b){
+    return a.second < b.second;
+}
+int maxChainLen(struct val vect[],int n){
+   if(n==0)return 0;
+   
+    int ans=1;
+    sort(vect,vect+n,comp);
+    int b=vect[0].second;
+    for(int i=1;i<n;i++){
+        // Condition for Compatibilty
+        if(b < vect[i].first){
+            b=vect[i].second;
+            ans++;
+        }
+    }
+    return ans;
+}    
