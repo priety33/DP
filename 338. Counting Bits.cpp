6c61ae36c08e9905fbf3a232b05bb84e4283e6ba
priety33 <52635177+priety33@users.noreply.github.com>
2020-04-13 00:37:32 +0530

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
+    
+    //Function to check whether the number is a power of 2 or now
+    inline bool ispowerof2(int n){
+        return (n & (n-1)) == 0;
+    }
+    
     vector<int> countBits(int num) {
-        vector<int> ans(num+1, 0);
-        for(int i=1; i<=num; i++)
+        vector<int> dp(num+1);
+        
+        dp[0]=0;
+        
+        if(num>=1) 
+            dp[1]=1;
+
+        int curr=2;
+        int nearest=2;
+        while(curr<=num)
         {
-            ans[i]=1+ ans[i&(i-1)];
+            //nearest stores the nearest power of 2 less than current element (nearest of 5 is 4, nearest of 13 is 8..)
+            nearest = ispowerof2(curr) ? curr  : nearest; 
+            dp[curr] = 1 + dp[curr-nearest]; // 1 stands for dp[nearest]
+            curr++;
         }
-        return ans;
+        
+        return dp;
     }
 };
