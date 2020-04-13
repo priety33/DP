4f849e97385d1c2a5e768c2f6d260d66a1cf7b03
priety33 <52635177+priety33@users.noreply.github.com>
2020-04-13 21:52:05 +0530

    Update 343. Integer Break.cpp

--- 343. Integer Break.cpp
+++ 343. Integer Break.cpp
@@ -1,17 +1,17 @@
+
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int> prod{0,1,1,2,4,6,9};
-        if(n<prod.size()) return prod[n];
-        for(int i=7;i<=n;i++)
-        {
-            int res=0;
-            for(int j=i-1; j>=1;j--)
-            {
-                if( (i-j)*prod[j]>res ) res=(i-j)*prod[j];
+        int dp[n+1];
+        memset(dp,INT_MIN,sizeof(dp));
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=i-1;j>=1;j--){
+                dp[i]=max(dp[i],max(dp[i-j]*j,(i-j)*j));
             }
-            prod.push_back(res);
         }
-        return prod[n];
+        //for(int i=0;i<=n;i++) cout << dp[i] <<" ";
+        return dp[n];
     }
 };
