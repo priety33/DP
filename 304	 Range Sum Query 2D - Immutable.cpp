e0bbe2e8e572b4cefa68792b492416b2add537e9
priety33 <52635177+priety33@users.noreply.github.com>
2020-04-14 16:41:44 +0530

    Create 	304	 Range Sum Query 2D - Immutable.cpp

---
+++ 304	 Range Sum Query 2D - Immutable.cpp
@@ -0,0 +1,31 @@
+class NumMatrix {
+private:
+    int row, col;
+    vector<vector<int>> sums;
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        row = matrix.size();
+        col = row>0 ? matrix[0].size() : 0;
+        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
+        for(int i=1; i<=row; i++) {
+            for(int j=1; j<=col; j++) {
+                sums[i][j] = matrix[i-1][j-1] + 
+                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+    }
+};
+
+/*
+Construct a 2D array sums[row+1][col+1]
+
+(notice: we add additional blank row sums[0][col+1]={0} and blank column sums[row+1][0]={0} 
+to remove the edge case checking), so, we can have the following definition
+
+sums[i+1][j+1] represents the sum of area from matrix[0][0] to matrix[i][j]
+
+https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/75350/Clean-C%2B%2B-Solution-and-Explaination-O(mn)-space-with-O(1)-time */
