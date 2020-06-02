0edba8dde968c17e564e127b6dba5d2a7fe716a9
priety33 <52635177+priety33@users.noreply.github.com>
2020-06-02 11:03:08 +0530

    Update 120. Triangle.cpp

--- 120. Triangle.cpp
+++ 120. Triangle.cpp
@@ -27,3 +27,17 @@ public:
         return ans;
     }
 };
+
+//using only O(n) space
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) 
+    {
+        int n= triangle.size();
+        vector<int> mini = triangle[n-1];
+        for ( int i = n - 2; i>= 0 ; --i )
+            for ( int j = 0; j < triangle[i].size() ; ++ j )
+                mini[j] = triangle[i][j] + min(mini[j],mini[j+1]);
+        return mini[0];
+    }
+};
